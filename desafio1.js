5f2dd074f014e8df139f14b9c5ef075b7c83b570
Romina Arce Aban <rarceaban@outlook.com>
2022-05-25 12:38:17 -0300

    first commit

---
+++ desafio1.js
@@ -0,0 +1,23 @@
+const { response } = require("express")
+const express = require("express")
+const { request } = require("http")
+
+const app = express()
+
+const server = app.listen(8080)
+
+app.get("/",(request, response) => {
+    response.send(
+        "<h1 style='color: blue;'>Bienvenidos a la super clase de servidores</h1>"
+    )
+})
+
+let count = 0
+app.get("/mvisitas", (request, response) => {
+    count++
+    response.send("La cantidad de visitas es:" + count)
+})
+app.get("/fyh", (request, response) => {
+    let fyh = new Date()
+    response.send({ fyh: fyh.toLocaleString()})
+})
